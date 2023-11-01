4620e067fee65fd7b4930893760ef42a84b8c3df
babacark <babacar.k@itdm-group.com>
2023-11-01 18:56:48 +0100

    auth and register

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/amazon'),
-    ProductModule],
+    ProductModule,
+    UserModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { NewUserDTO } from 'src/user/dtos/new-user.dto';
+import { UserDetails } from 'src/user/UserDetails.interface';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    @Post('register')
+    register(@Body() user: NewUserDTO): Promise<UserDetails | null>{
+        return this.authService.register(user);
+    }
+}
